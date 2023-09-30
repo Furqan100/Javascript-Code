36d9676eabbc247b3fe3cc900bb9157f68172987
Furqan100 <furqanlateef100@gmail.com>
2023-09-30 22:18:01 +0530

    practice object

---
+++ object.js
@@ -0,0 +1,11 @@
+const obj ={
+    name:"Furqan",
+    name2:"Ikhlaq",
+    rollNo : "21",
+    CLASS :"B-tech Cs"
+}
+console.log(obj["name"]);
+console.log(obj["rollNo"]);
+console.log(obj.name);
+console.log(obj.name2);
+console.log(obj.CLASS);
