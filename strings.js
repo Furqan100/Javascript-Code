bd341b8593c5af66ecb1beb6eefe0d715e32a3d7
Furqan100 <furqanlateef100@gmail.com>
2023-09-30 23:29:03 +0530

    strings completed

---
+++ strings.js
@@ -0,0 +1,18 @@
+let a ="Hellofurqanlateef";
+let b ="Furqan"
+console.log(`${a} my dear ${b}`)
+console.log(a.length)
+console.log( a.toUpperCase());
+console.log(a.charAt(4));
+console.log(a.indexOf('o'));
+let sub = a.slice(-4,-1)
+console.log(sub)
+// trim removes spaces
+let name1 = " furqan   "
+console.log(name1);
+console.log(name1.trim());
+//replace('','')
+//includes("hitesh")
+//convert string to Array
+console.log(a.split('-'));
+
