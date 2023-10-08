a89d42004ded4efa3ee878055e968659597893b2
Furqan100 <furqanlateef100@gmail.com>
2023-10-08 21:57:52 +0530

    revise objects datatypes,varletconst

---
+++ 02basics/varLetconst.js
@@ -0,0 +1,9 @@
+// prefer not to use var because of issue in global scoping and functional scoping
+let a ="Ikhlaq"
+let b ="Furqan"
+console.log(`${a} got fucked by ${b}`)
+let name1 = " furqan   "
+console.log(name1);
+//Trim method removes spaces from a variable
+console.log(name1.trim());
+console.table([a,b,name1]);
---
+++ Datatypes.js
@@ -0,0 +1,22 @@
+// "use strict" treats the code as the newer version of javascript
+"use strict";
+// alert(3+3 ) we are using nodejs not browser
+// js engine is hided inside the browser
+let b = 5
+console.log(`Azmat k naam mei ${b} letter hai`)
+let obj ={
+    name:"Azmat",
+    age:18,
+    address: "Moradabad",
+    course:"D-Pharmacy",
+    course_duration:"Two Years",
+    university:"TMU"
+
+
+}
+console.table([obj]);
+let a="azmat";
+let age =19;
+let college = "Pharmacy"
+var address = "Moradabad"
+console.table([a,college,address,age])
